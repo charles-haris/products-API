68d86bea4ecf3564fc216d7797e88a200f5e35c4
BOUASSE BU KOMBILE Charles-Haris <52077878+charles-haris@users.noreply.github.com>
2025-06-26 09:17:32 +0000

    Update main.js

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function closeCart_2(variable) {
     }
 }
 
-// charles-haris, BOUASSE
+// charles-haris, BOUASSE BU 
 // Saving to localStorage ()
 localStorage.setItem('cart', JSON.stringify({
     name: "kofi",
